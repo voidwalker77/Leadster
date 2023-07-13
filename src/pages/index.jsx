350e4fe214354028a411ca0797f15f0a36f2ece7
unknown <noccomposer@gmail.com>
2023-07-12 22:03:36 -0300

    Arruma o erro de build

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,27 @@
-import * as Styled from './styles'
+import styled from 'styled-components'
 import CallScreen from "../design-system/components/CallScreen/CallScreen";
 import Footer from '../design-system/components/Footer/Footer';
 import Header from '../design-system/components/Header/Header';
 import VideoSection from '../design-system/components/VideoSection/VideoSection';
 import Demo from '../design-system/components/DemoScreen/Demo';
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
 export default function Home() {
 	return (
 		<>
-			<Styled.Wrapper>
+			<Wrapper>
 				<Header />
 				<CallScreen />
 				<VideoSection />
 				<Demo />
 				<Footer />
-			</Styled.Wrapper>
+			</Wrapper>
 		</>
 	)
 }
